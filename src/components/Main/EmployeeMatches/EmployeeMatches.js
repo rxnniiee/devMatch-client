d02797510ee905de2b151a0036085b7cdda629dd
Staffan Strömsholm <52479640+StaffanStromsholm@users.noreply.github.com>
2021-04-15 10:20:17 +0300

    updated readme, created EmployeeMatches.js

---
+++ src/components/Main/EmployeeMatches/EmployeeMatches.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './EmployeeMatches.module.scss';
+
+const EmployeeMatches = () => {
+    return (
+        <div className={styles.EmployeeMatches}>
+            
+        </div>
+    )
+}
+
+export default EmployeeMatches;
