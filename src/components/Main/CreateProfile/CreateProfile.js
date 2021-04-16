cec4d08b5dc84936720f487cabd8a9829c21baf1
Sten Scheifel <sten.scheifel@gmail.com>
2021-04-16 12:37:20 +0300

    Create Profile Form

---
+++ src/components/Main/CreateProfile/CreateProfile.js
@@ -0,0 +1,125 @@
+import React from "react";
+import styles from "./CreateProfile.module.scss";
+import useInput from "../hooks/useInput";
+
+const validValue = (value) => value.trim() !== "";
+
+const CreateProfile = (props) => {
+  const {
+    value: entrName,
+    hasError: entrNameError,
+    isValid: nameValid,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: nameReset,
+  } = useInput(validValue);
+  const {
+    value: entrLname,
+    hasError: entrLnameError,
+    isValid: LnameValid,
+    valueChangeHandler: LnameChangeHandler,
+    inputBlurHandler: LnameBlurHandler,
+    reset: LnameReset,
+  } = useInput(validValue);
+  const {
+    value: entrDescribe,
+    hasError: entrDescribeError,
+    isValid: describeValid,
+    valueChangeHandler: describeChangeHandler,
+    inputBlurHandler: describeBlurHandler,
+    reset: describeReset,
+  } = useInput(validValue);
+  const {
+    value: entrSkill,
+    hasError: entrSkillError,
+    isValid: skillValid,
+    valueChangeHandler: skillChangeHandler,
+    inputBlurHandler: skillBlurHandler,
+    reset: skillReset,
+  } = useInput(validValue);
+  let FormIsValid = false;
+
+  if (nameValid && LnameValid && describeValid && skillValid) {
+    FormIsValid = true;
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!FormIsValid) {
+      return;
+    }
+
+    nameReset();
+    LnameReset();
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className={styles.control}>
+        <h1>Create Profile</h1>
+        <div className={styles.Form}>
+          <label htmlFor="name">First Name</label>
+          <input
+            id="fname"
+            type="text"
+            value={entrName}
+            onChange={nameChangeHandler}
+            onBlur={nameBlurHandler}
+          />
+          {entrNameError && (
+            <p className={styles.error}>Please enter yor name</p>
+          )}
+        </div>
+        <div className={styles.Form}>
+          <label htmlFor="name">Last Name</label>
+          <input
+            id="lname"
+            type="text"
+            value={entrLname}
+            onChange={LnameChangeHandler}
+            onBlur={LnameBlurHandler}
+          />
+          {entrLnameError && (
+            <p className={styles.error}>Please enter yor Last Name</p>
+          )}
+        </div>
+        <div className={styles.Form}>
+          <label htmlFor="name">Describe Yourself</label>
+          <textarea
+            id="describe"
+            type="text"
+            value={entrDescribe}
+            onChange={describeChangeHandler}
+            onBlur={describeBlurHandler}
+          />
+          {entrDescribeError && (
+            <p className={styles.error}>Please describe YourSelf</p>
+          )}
+        </div>
+        <div className={styles.Form}>
+          <label htmlFor="name">Skills</label>
+          <select
+            value={entrSkill}
+            onChange={skillChangeHandler}
+            onBlur={skillBlurHandler}
+          >
+            <option value="skill0">...</option>
+            <option value="skill1">Skill1</option>
+            <option value="skill2">Skill2</option>
+            <option value="skill3">Skill3</option>
+            <option value="skill4">Skill4</option>
+          </select>
+          {entrSkillError && (
+            <p className={styles.error}>Please choose a skill</p>
+          )}
+        </div>
+        <div>
+          <button disabled={!FormIsValid}>Create Profile</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default CreateProfile;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,39 +1,41 @@
-import React  from 'react';
-import { Route, Switch, } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 //components
-import LandingPage from './LandingPage/LandingPage';
-import Login from './Login/Login';
-import CreateAccount from './CreateAccount/CreateAccount';
-import CreateAccountCompany from './CreateAccount/CreateAccountCompany/CreateAccountCompany';
-import CreateAccountUser from './CreateAccount/CreateAccountUser/CreateAccountUser'
-
+import LandingPage from "./LandingPage/LandingPage";
+import Login from "./Login/Login";
+import CreateAccount from "./CreateAccount/CreateAccount";
+import CreateAccountCompany from "./CreateAccount/CreateAccountCompany/CreateAccountCompany";
+import CreateAccountUser from "./CreateAccount/CreateAccountUser/CreateAccountUser";
+import CreateProfile from "../Main/CreateProfile/CreateProfile";
 //styles
-import styles from './Main.module.scss';
+import styles from "./Main.module.scss";
 
 const Main = () => {
-    return (
-        <div className={styles.Main}>
-            <Switch>
-                <Route path="/" exact>
-                    <LandingPage />
-                </Route>
-                <Route path="/create-account" exact>
-                    <CreateAccount />
-                </Route>
-                <Route path="/create-account/company" exact>
-                    <CreateAccountCompany />
-                </Route>
-                <Route path="/create-account/user" exact>
-                    <CreateAccountUser />
-                </Route>
-                <Route path="/login">
-                    <Login />
-                </Route>
-                
-            </Switch>
-        </div>
-    )
-}
+  return (
+    <div className={styles.Main}>
+      <Switch>
+        <Route path="/" exact>
+          <LandingPage />
+        </Route>
+        <Route path="/create-account" exact>
+          <CreateAccount />
+        </Route>
+        <Route path="/create-account/company" exact>
+          <CreateAccountCompany />
+        </Route>
+        <Route path="/create-account/user" exact>
+          <CreateAccountUser />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/create-account/create-profile">
+          <CreateProfile />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
 
 export default Main;
