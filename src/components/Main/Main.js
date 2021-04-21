2a103a789ab41f6576428f6e59010e9792c2a8b9
anhletom <62256697+anhletom@users.noreply.github.com>
2021-04-21 12:53:29 +0300

    EeProfile_v1 (#16)

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,45 +2,53 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 //components
-import LandingPage from "./LandingPage/LandingPage";
-import Login from "./Login/Login";
-import CreateAccount from "./CreateAccount/CreateAccount";
-import CreateAccountCompany from "./CreateAccount/CreateAccountCompany/CreateAccountCompany";
-import CreateAccountUser from "./CreateAccount/CreateAccountUser/CreateAccountUser";
-import CreateProfile from "../Main/CreateProfile/CreateProfile";
-import EmployeeMatches from "./EmployeeMatches/EmployeeMatches";
+import LandingPage from './LandingPage/LandingPage';
+import Login from './Login/Login';
+import CreateAccount from './CreateAccount/CreateAccount';
+import CreateAccountCompany from './CreateAccount/CreateAccountCompany/CreateAccountCompany';
+import CreateAccountUser from './CreateAccount/CreateAccountUser/CreateAccountUser'
+import EmployeeMatches from './EmployeeMatches/EmployeeMatches';
+import Profile from '../EmployeeProfile/EeProfile'
 
 //styles
 import styles from "./Main.module.scss";
 
+const mockdata = {
+    name: "testname",
+    specicalty: "Fullstack",
+    jobType: "fulltime"
+
+}
+
 const Main = () => {
-  return (
-    <div className={styles.Main}>
-      <Switch>
-        <Route path="/" exact>
-          <LandingPage />
-        </Route>
-        <Route path="/create-account" exact>
-          <CreateAccount />
-        </Route>
-        <Route path="/create-account/company" exact>
-          <CreateAccountCompany />
-        </Route>
-        <Route path="/create-account/user" exact>
-          <CreateAccountUser />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/create-account/create-profile">
-          <CreateProfile />
-        </Route>
-        <Route path="/employee/matches">
-          <EmployeeMatches />
-        </Route>
-      </Switch>
-    </div>
-  );
-};
+    return (
+        <div className={styles.Main}>
+            <Switch>
+                <Route path="/" exact>
+                    <LandingPage />
+                </Route>
+                <Route path="/create-account" exact>
+                    <CreateAccount />
+                </Route>
+                <Route path="/create-account/company" exact>
+                    <CreateAccountCompany />
+                </Route>
+                <Route path="/create-account/user" exact>
+                    <CreateAccountUser />
+                </Route>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/employee/matches">
+                    <EmployeeMatches />
+                </Route>
+                <Route path="/profile">
+                    <Profile {...mockdata}/>
+                </Route>
+                
+            </Switch>
+        </div>
+    )
+}
 
 export default Main;
