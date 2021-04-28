ebb7fcfae20f47e0074e3002b035249e0a4f3ad1
Staffan Strömsholm <52479640+StaffanStromsholm@users.noreply.github.com>
2021-04-28 14:08:49 +0300

    refactored login logic

--- src/components/Main/Login/Login.js
+++ src/components/Main/Login/Login.js
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom';
 import styles from './Login.module.scss';
 
 const Login = () => {
+    
+    const [user, setUser] = useState();
 
-    const onSubmitHandler = () => {
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-        console.log('username: ' + username, 'password: ' + password);
+    const onChangeHandler = (e) => {
+        const { name, value } = e.target;
+        setUser({...user, [name]: value})
+    }
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        console.log(user);
     }
 
     return (
@@ -17,11 +23,11 @@ const Login = () => {
                 <h3>Sign In</h3>
                 <div className={styles.loginInputGroup}>
                     <label htmlFor="username">Username</label>
-                    <input required type="text" id="username" name="username"></input>
+                    <input  onChange={onChangeHandler} required type="text" id="username" name="username"></input>
                 </div>
                 <div className={styles.loginInputGroup}>
                     <label htmlFor="password">Password</label>
-                    <input required type="password" id="password" name="password"></input>
+                    <input onChange={onChangeHandler} required type="password" id="password" name="password"></input>
                 </div>
             </form>
             <Link to="/jobopening"><button onClick={onSubmitHandler}>Sign In</button></Link>
