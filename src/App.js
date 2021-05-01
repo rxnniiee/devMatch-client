cb015826fe9a472fcc7881cdbb66ca7de5c5050b
Stenbyte <sten.scheifel@gmail.com>
2021-05-01 17:14:41 +0300

    card component few fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 import Main from "./components/Main/Main";
+import Card from "./components/UI/Card";
 
 function App() {
   return (
